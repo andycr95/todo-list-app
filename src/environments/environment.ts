e928fb9e5aa09d753080dc6c40dc4d178ad1c6f0
andycr95 <andycr95@icloud.com>
2024-12-16 19:08:19 -0500

    Update dependencies and configure Firebase settings
    - Added @angular/fire
    - Updated environment.ts to include Firebase configuration settings for the application.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAb02wxkOOeUujwTWBxzABQ7fdJYv_skdw",
+    authDomain: "todo-list-app-bfe3f.firebaseapp.com",
+    projectId: "todo-list-app-bfe3f",
+    storageBucket: "todo-list-app-bfe3f.firebasestorage.app",
+    messagingSenderId: "878487123115",
+    appId: "1:878487123115:web:979d9940e5cae61e18f3ee"
+  }
 };
 
 /*
